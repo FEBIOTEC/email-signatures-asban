fb4a73073252ed329dbf0f863b31ce661ef5c16a
AleixMT <aleixaretra@gmail.com>
2024-03-21 17:00:40 +0100

    UPDATED: usbstitute marks

--- src/substituteMarks.sh
+++ src/substituteMarks.sh
@@ -3,12 +3,15 @@
 PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/../" &>/dev/null && pwd)"
 
 # Substitute each mark by its value using indirect expansion
-marks=["NAME" "PHONE" "IPHONE" "EMAIL"]
-for i in ${PROJECT_FOLDER}/data; do
-  initial="$(echo "${i}" | rev | cut -d "." -f1- | rev)"
+marks=("NAME" "PHONE" "IPHONE" "EMAIL")
+for i in ${PROJECT_FOLDER}/data/*; do
+  initial="$(echo "${i}" | rev | cut -d "/" -f1 | cut -d "." -f2-)"
   for mark in ${marks[@]}; do
     mark_name="${initial}_${mark}"
     mark_value=${!mark_name}
-    sed -i "s/{{${mark_name}}}/${mark_value}/g" "${PROJECT_FOLDER}/data/${i}"
+    if [ -n "${mark_value}" ]; then
+      echo jksdklfj
+      sed -i "s/{{${mark_name}}}/${mark_value}/g" "${i}"
+    fi
   done
 done
