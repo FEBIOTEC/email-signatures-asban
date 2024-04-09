3b532d979af0476c860bd4f0061cf014d0d3477a
AleixMT <aleixaretra@gmail.com>
2024-04-10 01:58:26 +0200

    trigger workflows

--- src/sendEmails.sh
+++ src/sendEmails.sh
@@ -5,7 +5,6 @@ PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/../" &>/dev/null && pwd)"
 for signature in $@; do
   pointer="$(echo "${signature}" | tr '[:lower:]' '[:upper:]')_EMAIL"
   email_value="${!pointer}"
-  echo $email_value
 
   curl -v --url 'smtps://smtp.gmail.com:465' \
     --ssl-reqd \
@@ -13,7 +12,18 @@ for signature in $@; do
     --mail-rcpt "${email_value}" \
     --user "${EMAIL_USERNAME}:${EMAIL_PASSWORD}" \
     -F '=(;type=multipart/mixed' \
-    -F "=Hola! Una nova signatura per al email d'ASBTEC ${email_value} ha estat generada. Descarrega el fitxer adjunt d'aquest correu i actualitza la teva signatura al teu client de correu electrònic. Si has rebut més d'un correu d'aquest tipus fes servir el contingut de l'últim mail per a actualitzar la firma, doncs serà la versió més actualitzada. Pots borrar la resta. Aquest missatge ha estat auto-generat. Per a qualsevol problema contacteu amb informatica@asbtec.cat. Fins aviat!  A;type=text/plain" \
+    -F "=Hola!
+
+Una nova signatura per al email d'ASBTEC ${email_value} ha estat generada. Descarrega el fitxer adjunt d'aquest correu i actualitza la teva signatura al teu client de correu electrònic.
+
+Si has rebut més d'un correu d'aquest tipus fes servir el contingut de l'últim mail per a actualitzar la firma, doncs serà la versió més actualitzada. Pots borrar la resta.
+
+Aquest missatge ha estat auto-generat. Per a qualsevol problema contacteu amb informatica@asbtec.cat.
+
+Fins aviat!
+
+A
+;type=text/plain" \
     -F "file=@${PROJECT_FOLDER}/out/${signature}.html;type=text/html;encoder=base64" \
     -F '=)' \
     -H "Subject: Actualització de signatures d'email" \
--- tools/setSecrets.sh
+++ tools/setSecrets.sh
@@ -5,6 +5,7 @@ PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/../" &>/dev/null && pwd)"
 set_secret()
 {
   gh secret set "$1" --body "$2" -r ASBTEC/email-signatures-asbtec
+  echo "with value: $2"
 }
 
 # Path to the file
@@ -19,6 +20,8 @@ fi
 # Open the file for reading
 while IFS= read -r line; do
   secret_name="$(echo "${line}" | cut -d "=" -f1)"
-  secret_content="$(echo "${line}" | cut -d "=" -f2- | sed 's/"*$//g')"
+  secret_content="$(echo "${line}" | cut -d "=" -f2-)"
+  secret_content="${secret_content%\"}"
+  secret_content="${secret_content#\"}"
   set_secret "${secret_name}" "${secret_content}"
 done < "$file"
