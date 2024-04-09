4e2a0ee2f8c703e3e7a6ec9e178bb60c02977fb5
AleixMT <aleixaretra@gmail.com>
2024-04-09 22:44:41 +0200

    meaningless changes to trigger workflows

--- src/sendEmails.sh
+++ src/sendEmails.sh
@@ -5,10 +5,7 @@ PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/../" &>/dev/null && pwd)"
 
 for signature in $@; do
   pointer="$(echo "${signature}" | tr '[:lower:]' '[:upper:]')_EMAIL"
-  echo pointer is $pointer
   email_value="${!pointer}"
-  echo value is $email_value
-  echo a secret is $INFORMATICS_EMAIL ${EMAIL_USERNAME}
   echo   curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from "${EMAIL_USERNAME}"  --mail-rcpt "${email_value}"  --user "${EMAIL_USERNAME}:${EMAIL_PASSWORD}"  --upload-file "${PROJECT_FOLDER}/out/${signature}.html"
   curl --url 'smtps://smtp.gmail.com:465' \
     --ssl-reqd \
