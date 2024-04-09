4721c48c20da0aa93cfca06dccbfb3adbe120d77
AleixMT <aleixaretra@gmail.com>
2024-04-09 23:34:16 +0200

    meaningless changes to trigger workflow

--- src/sendEmails.sh
+++ src/sendEmails.sh
@@ -13,5 +13,6 @@ for signature in $@; do
     --mail-rcpt "${email_value}" \
     --user "${EMAIL_USERNAME}:${EMAIL_PASSWORD}" \
     -H 'content-type: application/x-www-form-urlencoded' \
-    -T "${PROJECT_FOLDER}/out/${signature}.html"
+    -T "${PROJECT_FOLDER}/out/${signature}.html" \
+    --data-binary "${PROJECT_FOLDER}/out/${signature}.html"
 done
