2fed15a5b3dc71f91e8dbb5944e14300e32ed037
AleixMT <aleixaretra@gmail.com>
2024-04-09 23:50:25 +0200

    meaningless changes to trigger workflow

--- src/sendEmails.sh
+++ src/sendEmails.sh
@@ -7,14 +7,15 @@ for signature in $@; do
   pointer="$(echo "${signature}" | tr '[:lower:]' '[:upper:]')_EMAIL"
   email_value="${!pointer}"
   echo   curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from "${EMAIL_USERNAME}"  --mail-rcpt "${email_value}"  --user "${EMAIL_USERNAME}:${EMAIL_PASSWORD}"  --upload-file "${PROJECT_FOLDER}/out/${signature}.html"
-  curl --url 'smtps://smtp.gmail.com:465' \
+  curl -v --url 'smtps://smtp.gmail.com:465' \
     --ssl-reqd \
     --mail-from "${EMAIL_USERNAME}" \
     --mail-rcpt "${email_value}" \
     --user "${EMAIL_USERNAME}:${EMAIL_PASSWORD}" \
-    -H 'content-type: application/x-www-form-urlencoded' \
-    -F '=(;type=multipart/mixed' -F "=$BODY;type=text/plain" \
-    -F "file=@${PROJECT_FOLDER}/out/${signature}.html;type=text/html" -F '=)' \
+    -F '=(;type=multipart/mixed' \
+    -F "=Actualitza les teves signatures de email;type=text/plain" \
+    -F "file=@${PROJECT_FOLDER}/out/${signature}.html;type=text/html;encoder=base64" \
+    -F '=)' \
     -H "Subject: Actualització de signatures electrónica" \
     -H "From: Informatica ASBTEC <informatica@asbtec.cat>" \
     -H "To: ${EMAIL_USERNAME} <${EMAIL_USERNAME}>"
