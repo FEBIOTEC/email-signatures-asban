4fe89637ecbb12e952000318d36fdd6a26748264
AleixMT <aleixaretra@gmail.com>
2024-04-09 22:34:51 +0200

    meaningless changes to trigger workflows

--- src/substituteMarks.sh
+++ src/substituteMarks.sh
@@ -2,14 +2,13 @@
 
 PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")/../" &>/dev/null && pwd)"
 
-env
-
 # Substitute each mark by its value using indirect expansion
 marks=("NAME" "EMAIL" "POSITION" "LINKEDIN")
 for i in $@; do
-  initial="$(echo "${PROJECT_FOLDER}/data/${i}" | rev | cut -d "/" -f1 | cut -d "." -f2-)"
+  initial="$(echo "${PROJECT_FOLDER}/data/${i}" | rev | cut -d "/" -f1 | cut -d "." -f2- | tr '[:lower:]' '[:upper:]')"
   for mark in ${marks[@]}; do
-    mark_name="$(echo "${initial}" | tr '[:lower:]' '[:upper:]')_${mark}"
+    mark_name="${initial}_${mark}"
+    echo mark_name is $mark_name
     mark_value=${!mark_name}
     if [ -n "${mark_value}" ]; then
       echo "Substituted ${mark_name}"
